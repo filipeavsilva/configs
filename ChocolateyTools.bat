c0edffc935ce30857cdf138a4461b6ad5d7e1eb2
Filipe Silva <f.silva@coolblue.nl>
2015-08-19 10:38:19 +0200

    Make chocolatey install more streamlined

--- ChocolateyTools.bat
+++ ChocolateyTools.bat
@@ -1,20 +1,20 @@
 rem install stuff
-choco install git
-choco install ConEmu
-choco install winmerge
-choco install python
-choco install Everything
-choco install greenshot
-choco install putty
-choco install spotify
-choco install toastify
-choco install notepadplusplus
-choco install dropbox
-choco install keepass
-choco install vim
-choco install clipx
-choco install autohotkey
-choco install baretail
+choco install -y git
+choco install -y ConEmu
+choco install -y winmerge
+choco install -y python
+choco install -y Everything
+choco install -y greenshot
+choco install -y spotify
+choco install -y toastify
+choco install -y notepadplusplus
+choco install -y dropbox
+choco install -y keepass
+choco install -y vim
+choco install -y clipx
+choco install -y autohotkey
+choco install -y baretail
+choco install -y sharpkeys
 
 rem get my configs
 "%programfiles(x86)%\Git\bin\git" clone https://github.com/Darchangel/configs.git %homepath%\configs
@@ -22,3 +22,5 @@ mkdir %homepath%\.vim
 mklink /D %homepath%\vimfiles %homepath%\.vim
 mklink %homepath%\_vimrc %homepath%\configs\vimrc
 mklink %USERPROFILE%\Documents\Autohotkey.ahk %homepath%\configs\Autohotkey.ahk
+mklink %homepath%\.gitconfig %homepath%\configs\gitconfig 
+mklink %homepath%\.gitignore_global %homepath%\configs\gitignore_global
